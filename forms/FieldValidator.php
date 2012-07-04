2f008a8a3d4cbb300e9269a024cea101f2f5c023
Wojtek Szkutnik <wojtek.szkutnik@gmail.com>
2012-07-04 13:42:19 +0200

    Add docs to FieldValidator

--- forms/FieldValidator.php
+++ forms/FieldValidator.php
@@ -1,7 +1,23 @@
 <?php
-class FieldValidator
-{
-    function validate() {
+/**
+ * This validation class handles per-field validation rules.
+ * One field can be handled by multiple FieldValidators.
+ *
+ * Acts as a visitor to individual form fields.
+ *
+ * @package forms
+ * @subpackage validators
+ */
+class FieldValidator extends Object {
+
+    /**
+     * Determines whether a field is valid or not based on its value
+     *
+     * @param FormField $field Field to validate
+     * @param Validator $validator Validator class to use for error reporting
+     * @return bool
+     */
+    function validate($field, $validator) {
         return true;
     }
 }
